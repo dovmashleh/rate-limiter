c6465faa96170ee4850859a5a3a23fd08e8d6cb5
Денис Моторыгин <d.motorygin@qsoft.ru>
2025-03-09 15:29:57 +0300

    basic implementation

---
+++ limit.go
@@ -0,0 +1,50 @@
+package rate_limiter
+
+import (
+	"context"
+	"sync/atomic"
+	"time"
+)
+
+type limit[TKey comparable] struct {
+	amount atomic.Int32
+	next   atomic.Int64
+}
+
+func newLimit[TKey comparable](ctx context.Context, limiter *Limiter[TKey], key TKey) *limit[TKey] {
+	l := &limit[TKey]{}
+	ticker := time.NewTicker(time.Duration(limiter.period) * time.Millisecond)
+	l.next.Store(time.Now().UnixMilli())
+	l.run(ctx, ticker, limiter, key)
+	return l
+}
+
+func (l *limit[TKey]) run(ctx context.Context, ticker *time.Ticker, limiter *Limiter[TKey], key TKey) {
+	for {
+		select {
+		case <-ticker.C:
+			l.tick(ticker, limiter, key)
+		case <-ctx.Done():
+			ticker.Stop()
+			return
+		}
+	}
+}
+
+func (l *limit[TKey]) tick(ticker *time.Ticker, limiter *Limiter[TKey], key TKey) {
+	if l.amount.Add(-1) < 1 {
+		ticker.Stop()
+		limiter.remove(key)
+		return
+	}
+	l.next.Add(limiter.period)
+}
+
+func (l *limit[TKey]) call(maxBurst int32, period int64) (bool, time.Time) {
+	incAmount := l.amount.Add(1)
+	if incAmount > maxBurst {
+		l.amount.Add(-1)
+		return false, time.UnixMilli(l.next.Load())
+	}
+	return true, time.Time{}
+}
---
+++ limiter.go
@@ -0,0 +1,52 @@
+package rate_limiter
+
+import (
+	"context"
+	"math"
+	"sync"
+	"time"
+)
+
+type Limiter[TKey comparable] struct {
+	mu       *sync.RWMutex
+	limits   map[TKey]*limit[TKey]
+	maxBurst int32
+	period   int64
+}
+
+func New[TKey comparable](maxBurst int, periodMillis int) *Limiter[TKey] {
+	if maxBurst < 1 {
+		maxBurst = 1
+	}
+	if maxBurst > math.MaxInt32 {
+		panic("to large max burst")
+	}
+	if periodMillis < 1 {
+		periodMillis = 1
+	}
+	return &Limiter[TKey]{
+		mu:       &sync.RWMutex{},
+		limits:   make(map[TKey]*limit[TKey]),
+		maxBurst: int32(maxBurst),
+		period:   int64(periodMillis),
+	}
+}
+
+func (l *Limiter[TKey]) Try(ctx context.Context, key TKey) (can bool, waitUntil time.Time) {
+	l.mu.RLock()
+	lim, ok := l.limits[key]
+	l.mu.RUnlock()
+	if !ok {
+		lim = newLimit(ctx, l, key)
+		l.mu.Lock()
+		l.limits[key] = lim
+		l.mu.Unlock()
+	}
+	return lim.call(l.maxBurst, l.period)
+}
+
+func (l *Limiter[TKey]) remove(key TKey) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	delete(l.limits, key)
+}
